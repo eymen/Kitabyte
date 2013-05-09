f7bb4831bfbac9995ddc2108a35e197e3bd14719
Abdullah <abdsnr@gmail.com>
2013-05-09 12:02:58 +0300

    main toast eklendi

--- Kitabyte/src/com/mersoft/kitabyte/MainActivity.java
+++ Kitabyte/src/com/mersoft/kitabyte/MainActivity.java
@@ -3,6 +3,7 @@ package com.mersoft.kitabyte;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(getApplicationContext(), "metin", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
